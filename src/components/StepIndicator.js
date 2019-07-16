baa90efec21ccbc3e293f84e4b43308ded675282
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-16 15:53:26 -0300

    Hide form step indicator when keyboard show

--- src/components/StepIndicator.js
+++ src/components/StepIndicator.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { Keyboard } from 'react-native';
 import PropTypes from 'prop-types';
 import {
   StepIndicatorContainer,
   StepIndicatorCircle,
 } from './StepIndicator.styles';
 
-const StepIndicator = (props) => {
-  const { steps, currentIndex, stepIndicatorColor } = props;
-  return (
-    <StepIndicatorContainer>
-      {
-        steps.map((step, stepIndex) => {
-          const active = currentIndex === stepIndex;
-          // eslint-disable-next-line react/no-array-index-key
-          return (<StepIndicatorCircle key={stepIndex} active={active} stepIndicatorColor={stepIndicatorColor} />);
-        })
-      }
-    </StepIndicatorContainer>
-  );
+const initialState = {
+  visible: true,
 };
+
+class StepIndicator extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  componentWillMount() {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow.bind(this));
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+
+  handleKeyboardDidShow() {
+    return this.setState({ visible: false });
+  }
+
+  handleKeyboardDidHide() {
+    return this.setState({ visible: true });
+  }
+
+  render() {
+    const { visible } = this.state;
+    const {
+      steps,
+      currentIndex,
+      stepIndicatorColor,
+    } = this.props;
+    return visible && (
+      <StepIndicatorContainer>
+        {
+          steps.map((step, stepIndex) => {
+            const active = currentIndex === stepIndex;
+            // eslint-disable-next-line react/no-array-index-key
+            return (<StepIndicatorCircle key={stepIndex} active={active} stepIndicatorColor={stepIndicatorColor} />);
+          })
+        }
+      </StepIndicatorContainer>
+    );
+  }
+}
 
 StepIndicator.defaultProps = {
   steps: [],
