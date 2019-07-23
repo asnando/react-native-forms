1c2945239dd11aebc84880624b5c9575c09df4cb
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-23 18:22:47 -0300

    Update package dependencies

--- examples/RNFormsPlayground/App.js
+++ examples/RNFormsPlayground/App.js
@@ -1,14 +1,74 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
 } from 'react-native';
+import {
+  Form,
+  FormTabs,
+  FormTab,
+  FormSteps,
+  FormStep,
+  FormView,
+  TextInput,
+} from 'react-native-forms';
+
+const renderFormFields = () => {
+  return (
+    <Fragment>
+      <Text>{Date.now()} - Random text inside form</Text>
+      <TextInput title="Test" />
+    </Fragment>
+  );
+};
+
+const renderFormTabs = () => {
+  return (
+    <FormTabs tabIndicatorColor="green">
+      <FormTab title="First">
+        <FormView>{renderFormFields()}</FormView>
+      </FormTab>
+      <FormTab title="Second">
+        <FormView>{renderFormFields()}</FormView>
+      </FormTab>
+    </FormTabs>
+  );
+};
+
+const renderFormSteps = () => {
+  return (
+    <FormSteps>
+      <FormStep title="First Step">
+        <FormView>{renderFormFields()}</FormView>
+      </FormStep>
+      <FormStep title="Second Step">
+        <FormView>{renderFormFields()}</FormView>
+      </FormStep>
+    </FormSteps>
+  );
+};
+
+const renderFormWithTabs = () => (
+  <Form style={styles.form}>{renderFormTabs()}</Form>
+);
+
+const renderFormWithSteps = () => (
+  <Form style={styles.form}>{renderFormSteps()}</Form>
+);
+
+const renderForms = () => (
+  <Fragment>
+    {renderFormWithTabs()}
+    {renderFormWithSteps()}
+  </Fragment>
+);
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>RNFormsPlayground</Text>
+      {/* {renderForms()} */}
+      {renderFormWithSteps()}
     </SafeAreaView>
   );
 };
@@ -16,9 +76,13 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f0f0f0',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  form: {
+    
+  }
 });
 
 export default App;
