70217e8e4cc62d7b84e4ff52b14608fd06a6bc9c
Fernando Rama <fernandorama.dev@gmail.com>
2019-07-23 11:46:40 -0300

    Reset example

--- examples/RNFormsPlayground/App.js
+++ examples/RNFormsPlayground/App.js
@@ -1,27 +1,21 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
-  StatusBar,
 } from 'react-native';
-// import {} from 'react-native-forms';
 
 const App = () => {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <Text>RNFormsPlayGround</Text>
-      </SafeAreaView>
-    </Fragment>
+    <SafeAreaView style={styles.container}>
+      <Text>RNFormsPlayground</Text>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '90%',
     alignItems: 'center',
     justifyContent: 'center',
   },
